d6f9d7ed5d4d3be7d250cd0d28c1fe195faba024
Tiana Hayden <tiana.hayden@me.com>
2018-09-27 12:06:35 -0700

    Added bootstrap styling and the layout for rows in the table

--- src/app.jsx
+++ src/app.jsx
@@ -2,8 +2,52 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
+    var headerStyle = {
+      width: '80vw',
+      margin: '5vw 1vw 2vw 1vw',
+    }
+
+    var cardStyle = {
+      width: '80vw',
+      margin: '1vw',
+    }
+
     return (
-      <div className='App'></div>
+      <div className='App'>
+        <div className='container'>
+          <div className='row justify-content-center'>
+            <div class="col">
+              <div className='card' style={headerStyle}>
+                <div className='card-body text-center'>
+                  <h1>San Diego Top Spots</h1>
+                  <p>A list of 30 places to see in San Diego, California.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='row justify-content-center'>
+            <div class="col">
+              <div className='card' style={cardStyle}>
+                <div className='card-body'>
+                  <p>Next item</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='row justify-content-center'>
+            <div class="col">
+              <div className='card' style={cardStyle}>
+                <div className='card-body'>
+                  <p>Next item</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
     );
   }
 }
